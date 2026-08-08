593665de7d3a6d7621baa3202bee5347e6be8251
Sharshti Singhai <singhaisharshti@gmail.com>
2026-08-08 20:59:09 +0530

    Add files via upload

---
+++ static/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "askai-v1";
+
+self.addEventListener("install", function () {
+
+    console.log("AskAI Service Worker installed");
+
+});
+
+
+self.addEventListener("activate", function () {
+
+    console.log("AskAI Service Worker activated");
+
+});
+
+
+self.addEventListener("fetch", function (event) {
+
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
+        })
+    );
+
+});
